e212bf6f89ddb66b23f03a18f3fe4e49ee13baaf
André <algc321@gmail.com>
2019-08-06 15:50:54 -0300

    backend

---
+++ backend/src/controllers/LikeController.js
@@ -0,0 +1,24 @@
+const Dev = require('../models/Dev');
+
+module.exports = {
+    async store(req, res) {
+
+        const { user } = req.headers;
+        const { devId } = req.params;
+
+
+        const loggedDev = await Dev.findById(user);
+        const targetDev = await Dev.findById(devId);
+
+        if (!targetDev) {
+            return res.status(400).json({ error: 'Dev not find.' });
+        }
+        if (targetDev.likes.includes(user)) {
+            console.log('Deu Match!');
+        }
+        loggedDev.likes.push(targetDev._id);
+
+        await loggedDev.save();
+        return res.json(loggedDev);
+    }
+};
